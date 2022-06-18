5e370c26c9afed6965c83248d0a40d562fcce22b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-06-18 09:13:00 +0700

    add new security config

--- src/main/java/com/demo/security/SecurityConfig.java
+++ src/main/java/com/demo/security/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.demo.security.filter.CustomUsernamePasswordFilter;
+import com.demo.security.filter.JWTFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -22,6 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().permitAll();
+		// add filter or change filter with custom filter
+		http.addFilterAt(new CustomUsernamePasswordFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.addFilterAfter(new JWTFilter(), CustomUsernamePasswordFilter.class);
 		http.csrf().disable();
 		http.cors(c -> {
 			CorsConfigurationSource src = (request) -> {
