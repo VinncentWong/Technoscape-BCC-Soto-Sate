30aeccf9211856c89f268b3534a4a8450b8a8d85
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-06-18 09:09:40 +0700

    add jwt generate token util

--- src/main/java/com/demo/util/JWTUtil.java
+++ src/main/java/com/demo/util/JWTUtil.java
@@ -1,5 +1,26 @@
 package com.demo.util;
 
-public class JWTUtil {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
+import com.demo.entities.User;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtil {
+	
+	public String generateToken(User user) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", user.getId());
+		map.put("username", user.getUsername());
+		map.put("email", user.getEmail());
+		String token = Jwts.builder()
+							.setSubject(user.getUsername())
+							.setClaims(map)
+							.compact();
+		return token;
+	}
 }
