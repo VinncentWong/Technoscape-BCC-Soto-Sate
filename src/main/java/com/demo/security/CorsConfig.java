13d884162ee72396eda96a6e25f8e007c3e8d078
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-06-19 11:58:59 +0700

    comment and uncomment

--- src/main/java/com/demo/security/CorsConfig.java
+++ src/main/java/com/demo/security/CorsConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer{
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//				.allowedHeaders("*")
-//				.allowedOrigins("http://localhost:3000/")
-//				.allowedMethods("GET", "PUT", "PATCH", "POST", "DELETE");
-//	}
-//}
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedHeaders("*")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "PUT", "PATCH", "POST", "DELETE");
+	}
+}
