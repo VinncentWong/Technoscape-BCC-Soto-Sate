e2072a20088f831462eb477bd866bcbe04992965
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-06-19 16:03:04 +0700

    change cors filter constructor

--- src/main/java/com/demo/security/SecurityConfig.java
+++ src/main/java/com/demo/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.demo.security;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.catalina.filters.CorsFilter;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.demo.security.filter.CustomUsernamePasswordFilter;
 import com.demo.security.filter.JWTFilter;
